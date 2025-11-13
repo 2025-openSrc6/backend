c67d7ea6599994cd975103e6afae71ca1a6e5191
ehdnd_mac <taeungjang@naver.com>
2025-11-13 11:59:28 +0900

    chore: 기본 ci 코드 품질 검사 파이프라인 구축

---
+++ scripts/mock-google-fonts.cjs
@@ -0,0 +1,20 @@
+module.exports = {
+  'https://fonts.googleapis.com/css2?family=Geist:wght@100..900&display=swap': `/* Mocked Geist font */
+@font-face {
+  font-family: 'Geist';
+  font-style: normal;
+  font-weight: 400;
+  font-display: swap;
+  src: local('Arial');
+}
+`,
+  'https://fonts.googleapis.com/css2?family=Geist+Mono:wght@100..900&display=swap': `/* Mocked Geist Mono font */
+@font-face {
+  font-family: 'Geist Mono';
+  font-style: normal;
+  font-weight: 400;
+  font-display: swap;
+  src: local('Courier New');
+}
+`,
+};
